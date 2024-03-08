9debd147ce1f111bd995909ed34df8cf1dc9fa0c
Pedro Dantas <pedrodantaspc@gmail.com>
2024-03-07 21:01:11 -0300

    adicionado o alerta nas falhas fftx

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function teste(){
   
-/*verifica se o input radio ftth esta selecionado*/ const redeftth = document.getElementById('ftth').checked
-/*verifica se o input radio fttx esta selecionado*/ const redefttx = document.getElementById('fttx').checked
+ const redeftth = document.getElementById('ftth').checked
+const redefttx = document.getElementById('fttx').checked
 
 const cidadeFalha = document.getElementById('cidade_falha').value
 const bairroFalha = document.getElementById('bairro_falha').value
@@ -22,25 +22,35 @@ alertafalha.innerHTML=`FALHA DE REDE LOCAL - ${bairroFalha} - ${slotPonFalha} </
 ID ${idFalha} </br></br>
 CLIENTES QUE RESIDEM  EM ${bairroFalha} E SE CONECTAM AO ${slotPonFalha}, IRÃO SENTIR FALTA DE ACESSO DEVIDO À UM ROMPIMENTO DE FIBRA.
 NOSSA EQUIPE JÁ ESTÁ CIENTE E FAZENDO O POSSÍVEL PARA NORMALIZAR O QUANTO ANTES. </br></br>
-PRAZO PARA NORMALIZAÇÃO:${prazoFalha} </br></br>
-INÍCIO DA FALHA:${inicioFalha}`
+PRAZO PARA NORMALIZAÇÃO: ${prazoFalha} </br></br>
+INÍCIO DA FALHA: ${inicioFalha}`
 
 
 msgplantao.innerHTML=`IDENTIFICADOS CLIENTES SEM ACESSO  ${bairroFalha} - ${slotPonFalha}</br></br>
-RUA ${ruaFalha}</br></br>
+RUA(S) ${ruaFalha}</br></br>
 MEDIA DE ${mediaOff} CLIENTES </br></br>
 PRAZO INFORMADO ATE ${prazoFalha}`
 
 
 }else if(redefttx){
 
-  alertafalha.innerHTML=``
-  msgplantao.innerHTML=``
+  alertafalha.innerHTML=`FALHA DE REDE LOCAL - ${bairroFalha} - ${ruaFalha} </br></br>
+  ID ${idFalha} </br></br>
+  CLIENTES QUE RESIDEM  EM ${bairroFalha}, RUA(S) ${ruaFalha}, IRÃO SENTIR FALTA DE ACESSO DEVIDO À UMA FALHA NO EQUIPAMENTO DA LOCALIDADE.
+  NOSSA EQUIPE JÁ ESTÁ CIENTE E FAZENDO O POSSÍVEL PARA NORMALIZAR O QUANTO ANTES. </br></br>
+  PRAZO PARA NORMALIZAÇÃO: ${prazoFalha} </br></br>
+  INÍCIO DA FALHA: ${inicioFalha}`
+  
+  
+  msgplantao.innerHTML=`IDENTIFICADOS CLIENTES SEM ACESSO  ${bairroFalha}</br></br>
+  RUA(S) ${ruaFalha}</br></br>
+  MEDIA DE ${mediaOff} CLIENTES </br></br>
+  PRAZO INFORMADO ATE ${prazoFalha}`
 
 }else{
 
-  alertafalha.innerHTML=``
-  msgplantao.innerHTML=``
+  alertafalha.innerHTML=`SELECIONE FTTH OU FTTX`
+ 
 
 }
 }
